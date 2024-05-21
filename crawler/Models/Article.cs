cc33f1880fb1cb6740bec3baa1b6287673e41648
Jason Chen <js910924@gmail.com>
2024-05-21 17:59:40 +0800

    feat: user able to search articles by keyword

---
+++ crawler/Models/Article.cs
@@ -0,0 +1,9 @@
+namespace crawler.Models;
+
+public class Article
+{
+    public string Title { get; set; }
+    public string Link { get; set; }
+    public DateTime Date { get; set; }
+    public string Author { get; set; }
+}
