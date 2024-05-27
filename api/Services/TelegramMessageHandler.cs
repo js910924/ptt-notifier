69ae5aeab711895c516d380d8ec403e3dc20e92a
Jason Chen <js910924@gmail.com>
2024-05-27 12:54:55 +0800

    chore: use string Equals method

--- api/Services/TelegramMessageHandler.cs
+++ api/Services/TelegramMessageHandler.cs
@@ -25,74 +25,85 @@ public class TelegramMessageHandler : ITelegramMessageHandler
 
     public async Task Handle(long chatId, string message)
     {
+        var replyText = string.Empty;
         if (message.StartsWith("/subscribe"))
         {
-            var messageText = message.Split(' ');
-            if (messageText.Length == 3 && messageText[0].ToLower() == "/subscribe")
-            {
-                // TODO: validate is valid board 
-                var board = messageText[1];
-                var keyword = messageText[2];
-    
-                await _subscriptionRepository.Add(chatId, board, keyword);
-                var subscribedBoards = await _subscribedBoardRepository.GetAll();
-                if (!subscribedBoards.Exists(b => b.Board == board))
-                {
-                    var latestArticle = await _pttClient.GetLatestArticle(board);
-                    var subscribedBoard = new SubscribedBoard
-                    {
-                        Board = board,
-                        LastLatestArticleTitle = latestArticle.Title
-                    };
-                    await _subscribedBoardRepository.Add(subscribedBoard);
-                }
-    
-                await _telegramBotClient.SendTextMessageAsync(chatId, "Subscribe successfully.");
-            }
-            else
-            {
-                await _telegramBotClient.SendTextMessageAsync(chatId, "Invalid command. Use /subscribe [board] [keyword]");
-            }
+            replyText = await Subscribe(chatId, message);
         }
         else if (message.StartsWith("/unsubscribe"))
         {
-            var messageText = message.Split(' ');
-            if (messageText.Length == 3 && messageText[0].ToLower() == "/unsubscribe")
-            {
-                var board = messageText[1];
-                var keyword = messageText[2];
-    
-                await _subscriptionRepository.Delete(chatId, board, keyword);
-                var subscriptions = await _subscriptionRepository.GetAll();
-                if (subscriptions.TrueForAll(subscription => subscription.Board != board))
-                {
-                    await _subscribedBoardRepository.Delete(board);
-                    await _articleRepository.Delete(board);
-                }
-    
-                await _telegramBotClient.SendTextMessageAsync(chatId,
-                    "Unsubscribe successfully.");
-            }
-            else
-            {
-                await _telegramBotClient.SendTextMessageAsync(chatId,
-                    "Invalid command. Use /unsubscribe [board] [keyword]");
-            }
+            replyText = await Unsubscribe(chatId, message);
         }
         else if (message.Equals("/list", StringComparison.OrdinalIgnoreCase))
         {
-            try
-            {
-                var subscriptions = await _subscriptionRepository.Get(chatId);
-                var text = string.Join('\n', subscriptions.Select(subscription => $"{subscription.Board} {subscription.Keyword}"));
-
-                await _telegramBotClient.SendTextMessageAsync(chatId, text);
-            }
-            catch (Exception e)
-            {
-                await _telegramBotClient.SendTextMessageAsync(chatId, "Unexpected error");
-                _logger.LogError(e, "List subscriptions failed");
-            }
+            replyText = await ListSubscription(chatId);
         }
+
+        await _telegramBotClient.SendTextMessageAsync(chatId, replyText);
+    }
+
+    private async Task<string> ListSubscription(long chatId)
+    {
+        try
+        {
+            var subscriptions = await _subscriptionRepository.Get(chatId);
+
+            return string.Join('\n', subscriptions.Select(subscription => $"{subscription.Board} {subscription.Keyword}"));
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "List subscriptions failed");
+            return "Unexpected error";
+        }
+    }
+
+    private async Task<string> Unsubscribe(long chatId, string message)
+    {
+        var messageText = message.Split(' ');
+        if (messageText.Length == 3 && messageText[0].Equals("/unsubscribe", StringComparison.CurrentCultureIgnoreCase))
+        {
+            var board = messageText[1];
+            var keyword = messageText[2];
+    
+            await _subscriptionRepository.Delete(chatId, board, keyword);
+            var subscriptions = await _subscriptionRepository.GetAll();
+            if (subscriptions.TrueForAll(subscription => subscription.Board != board))
+            {
+                await _subscribedBoardRepository.Delete(board);
+                await _articleRepository.Delete(board);
+            }
+
+            return "Unsubscribe successfully.";
+        }
+
+        return "Invalid command. Use /unsubscribe [board] [keyword]";
+    }
+
+    private async Task<string> Subscribe(long chatId, string message)
+    {
+        var messageText = message.Split(' ');
+        if (messageText.Length == 3 && messageText[0].Equals("/subscribe", StringComparison.CurrentCultureIgnoreCase))
+        {
+            // TODO: validate is valid board 
+            var board = messageText[1];
+            var keyword = messageText[2];
+    
+            await _subscriptionRepository.Add(chatId, board, keyword);
+            var subscribedBoards = await _subscribedBoardRepository.GetAll();
+            if (!subscribedBoards.Exists(b => b.Board == board))
+            {
+                var latestArticle = await _pttClient.GetLatestArticle(board);
+                var subscribedBoard = new SubscribedBoard
+                {
+                    Board = board,
+                    LastLatestArticleTitle = latestArticle.Title
+                };
+                await _subscribedBoardRepository.Add(subscribedBoard);
+            }
+
+            return "Subscribe successfully.";
+        }
+
+        return "Invalid command. Use /subscribe [board] [keyword]";
     }
 }
