e8e84326a6863475161095d395f1321f662f5e56
Jason Chen <js910924@gmail.com>
2024-06-29 15:57:46 +0800

    refactor: move TelegramUrl to ctor

--- api/SetTelegramWebhookHostedService.cs
+++ api/SetTelegramWebhookHostedService.cs
@@ -8,16 +8,20 @@ public class SetTelegramWebhookHostedService : IHostedService
     private readonly HttpClient _httpClient;
     private readonly TelegramConfig _telegramConfig;
 
-    public SetTelegramWebhookHostedService(HttpClient httpClient, IOptions<TelegramConfig> options)
+    public SetTelegramWebhookHostedService(IHttpClientFactory httpClientFactory, IOptions<TelegramConfig> options)
     {
+        var httpClient = httpClientFactory.CreateClient();
+        var telegramConfig = options.Value;
+
+        httpClient.BaseAddress = new Uri(telegramConfig.TelegramUrl);
         _httpClient = httpClient;
-        _telegramConfig = options.Value;
+        _telegramConfig = telegramConfig;
     }
 
     public async Task StartAsync(CancellationToken cancellationToken)
     {
-        var setWebhookUrl = $"{_telegramConfig.TelegramUrl}/bot{_telegramConfig.Token}/setwebhook?url={_telegramConfig.WebhookUrl}";
-        await _httpClient.GetAsync(setWebhookUrl, cancellationToken);
+        var setWebhookEndpoint = $"/bot{_telegramConfig.Token}/setwebhook?url={_telegramConfig.WebhookUrl}";
+        await _httpClient.GetAsync(setWebhookEndpoint, cancellationToken);
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
